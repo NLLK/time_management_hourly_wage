df8c86e746c3320e825444c935aa2e4ba3dd18d0
Barry Allen <babanov-artem@yandex.ru>
2022-12-29 22:36:20 +0600

    add getattr test and start point of app

--- menu.py
+++ menu.py
@@ -1,3 +1,8 @@
+import options
+
 class Menu:
     def showMenu():
+        print(hasattr(options.Options, "simple_td_t1_t2")) #check if this module have this function
+        bar = getattr(options.Options, "simple_td_t1_t2")  #run modules function
+        bar()
         pass
--- options.py
+++ options.py
@@ -7,6 +7,7 @@ class Options:
         description = ""
 
     def simple_td_t1_t2():
+        print('abbbbbboooooobbbbbbbaaaaaaaaa')
         pass
     def simple_td_now_t2():
         pass
